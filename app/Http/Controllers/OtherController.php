a6c6a1021913ed4aa09be8b15ea93d493f25d0e5
games647 <games@tsprivat.de>
2016-06-17 10:58:45 +0200

    Add caching the domain resolve

--- app/Http/Controllers/OtherController.php
+++ app/Http/Controllers/OtherController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use \App\Player;
 use \Cache;
 
 class OtherController extends ApiController
 {
     public function domainRecords($domain)
     {
+        $cached = Cache::get('domain:' . $domain);
+        if ($cached !== NULL) {
+            return collect($cached)->put('source', 'cache');
+        }
+
 	if (ip2long($domain) !== FALSE) {
 	    //it's not a domain it's a IP-Address which we cannot resolve
 	    return response('', 400);
 	}
 
-	return dns_get_record($domain);
+        $records = collect(dns_get_record($domain))->put('updated_at', time());
+        Cache::put('domain:' . $domain, $records, env('CACHE_LENGTH', 10));
+	return $records;
     }
 }
